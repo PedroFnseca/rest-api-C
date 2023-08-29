ac10dd826139b13866bf5126f64d8b1b81452939
pedrofnseca <pedropereira.pp264@gmail.com>
2023-08-29 05:12:48 -0300

    feat: add protection to run

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p bin
 
 gcc -o bin/main src/main.c -lmicrohttpd
-
-if [ $? -eq 0 ]; then
-    echo "Compilação bem-sucedida."
-else
-    echo "Erro durante a compilação."
-fi
--- main_run.sh
+++ main_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ./build.sh
 
 echo "Executando o programa..."
