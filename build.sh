4b9fdce678f3562bac086a10b10505073cccb463
pedrofnseca <pedropereira.pp264@gmail.com>
2023-08-29 01:07:25 -0300

    remove comments

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Cria a pasta bin se não existir
 mkdir -p bin
 
-# Compila o código da pasta src usando gcc e libmicrohttpd, e coloca o executável na pasta bin
 gcc -o bin/main src/main.c -lmicrohttpd
 
-# Verifica se a compilação foi bem-sucedida
 if [ $? -eq 0 ]; then
     echo "Compilação bem-sucedida."
 else
